ddcb9a5e0fab55d37bbd0e2c657b66d5745a8efa
Drishti Singh <59653688+drishh207@users.noreply.github.com>
2021-03-13 00:19:49 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("navbar").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("navbar").style.marginRight= "0";
+}
